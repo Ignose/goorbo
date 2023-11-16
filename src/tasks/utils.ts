2b1584144ef1dc057f0fbf5a11a68d566f207e72
Ignose <ihpares@gmail.com>
2023-11-16 11:12:39 -0500

    Smol update

--- src/tasks/utils.ts
+++ src/tasks/utils.ts
@@ -11,18 +11,15 @@ import {
   mallPrice,
   Monster,
   myAdventures,
-  myClass,
   myFamiliar,
   myFullness,
   myInebriety,
-  myLevel,
   mySpleenUse,
   retrieveItem,
   spleenLimit,
   use,
 } from "kolmafia";
 import {
-  $class,
   $familiar,
   $familiars,
   $item,
@@ -34,7 +31,6 @@ import {
   Snapper,
 } from "libram";
 import { garboAverageValue, garboValue } from "../engine/profits";
-import { args } from "../args";
 
 export function haveAll(its: Item[]): boolean {
   return its.reduce((a, it) => a && have(it), true);
@@ -99,11 +95,6 @@ export function maxBase(): string {
 
 function famValue(fam: Familiar, mob?: Monster) {
   switch (fam) {
-    case $familiar`Grey Goose`:
-      return (myClass() === $class`Grey Goo` || myLevel() < args.targetlevel) &&
-        $familiar`Grey Goose`.experience < 400
-        ? 6000
-        : 0;
     case $familiar`Red-Nosed Snapper`:
       if (mob && Snapper.getTrackedPhylum() && mob.phylum === Snapper.getTrackedPhylum())
         return (
